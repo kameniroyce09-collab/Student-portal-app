314ab3655220d86d35c4ad89fc093387226e2b7a
Arsene7 <arsenet7@gmail.com>
2025-12-17 23:27:57 +0000

    IP address  modified

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // CORS configuration - MUST be before other middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://18.118.140.140:3000', 'http://18.118.140.140:5000'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://3.145.9.67:3000', 'http://3.145.9.67:5000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
