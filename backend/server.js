7c4570f58a479ecde1a4b6c71c8c561d7e22ac0b
Arsene7 <arsenet7@gmail.com>
2026-01-02 10:26:17 +0000

    server.js modified

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // CORS configuration - MUST be before other middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://18.118.27.1:3000',  'http://18.118.27.1:5000'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://3.137.222.241:3000',  'http://3.137.222.241:5000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
